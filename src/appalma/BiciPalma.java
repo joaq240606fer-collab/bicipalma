527e91e63dc349bf477359ceb3b2b37d3a362308
joaq240606fer-collab <joaq240606fer@gmail.com>
2026-04-07 19:21:48 +0200

    feat(Bicipalma): :sparkles: Crear clase main

---
+++ src/appalma/BiciPalma.java
@@ -0,0 +1,64 @@
+package appalma;
+
+import domain.bicicleta.Bicicleta;
+import domain.estacion.Estacion;
+import domain.tarjetausuario.TarjetaUsuario;
+
+public class BiciPalma {
+
+	public static void main(String[] args) {
+
+		Estacion estacion = new Estacion(1, "Manacor", 6);
+
+		System.out.println("\n **** caso TEST visualizar estado de la estacion **** \n");
+
+		estacion.consultarEstacion();
+
+		System.out.println("\n **** caso TEST visualizar anclajes libres **** \n");
+
+		System.out.println("anclajesLibres: " + estacion.anclajesLibres());
+
+		estacion.consultarAnclajes();
+
+		System.out.println("\n **** caso TEST anclar bicicleta(s) **** \n");
+
+		int[] bicicletas = { 291, 292, 293, 294 };
+		
+		Bicicleta bicicleta = null;
+		for (int id : bicicletas) {
+			bicicleta = new Bicicleta(id);
+			estacion.anclarBicicleta(bicicleta);
+		}
+
+		System.out.println("anclajes libres tras generar " + bicicletas.length 
+							+ " bicis: " + estacion.anclajesLibres());
+
+		System.out.println("\n **** caso TEST consultar bicicletas ancladas **** \n");
+
+		estacion.consultarAnclajes();
+
+		System.out.println("\n **** caso TEST retirar bicicleta **** \n");
+
+		TarjetaUsuario tarjetaUsuario = new TarjetaUsuario("000456789", true);
+
+		System.out.println("¿tarjeta de usuario activada? (true/false): " 
+							+ estacion.leerTarjetaUsuario(tarjetaUsuario));
+
+		estacion.retirarBicicleta(tarjetaUsuario);
+
+		estacion.consultarAnclajes();
+
+		System.out.println("anclajesLibres: " + estacion.anclajesLibres());
+
+		System.out.println("\n **** caso TEST tarjeta inactiva **** \n");
+
+		tarjetaUsuario.setActivada(false);
+
+		System.out.println("¿tarjeta de usuario activada? (true/false): " 
+							+ estacion.leerTarjetaUsuario(tarjetaUsuario));
+
+		estacion.retirarBicicleta(tarjetaUsuario);
+
+		estacion.consultarAnclajes();
+	}
+}
